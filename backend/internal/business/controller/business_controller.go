56774f00033f69d0460c4bbef146c4efa852c3c0
smartx-web <musabilyamin137@gmail.com>
2026-07-30 16:31:53 +0100

    business_controller.go

--- backend/internal/business/controller/business_controller.go
+++ backend/internal/business/controller/business_controller.go
@@ -4,26 +4,37 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/smartx-web/idoma-connect/backend/internal/business/model"
+	"github.com/smartx-web/idoma-connect/backend/internal/business/repository"
 )
 
 func GetBusinesses(c *gin.Context) {
-	businesses := []gin.H{
-		{
-			"id":       1,
-			"name":     "Royal Hospital",
-			"category": "Hospital",
-			"lga":      "Otukpo",
-		},
-		{
-			"id":       2,
-			"name":     "City Hotel",
-			"category": "Hotel",
-			"lga":      "Otukpo",
-		},
-	}
-
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"data":    businesses,
+		"data": repository.Businesses,
+	})
+}
+
+func CreateBusiness(c *gin.Context) {
+
+	var business model.Business
+
+	if err := c.ShouldBindJSON(&business); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "Invalid request",
+		})
+		return
+	}
+
+	business.ID = uint(len(repository.Businesses) + 1)
+
+	repository.Businesses = append(repository.Businesses, business)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"success": true,
+		"message": "Business created successfully",
+		"data": business,
 	})
 }
