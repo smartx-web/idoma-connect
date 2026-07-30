4e5b5c7dfe7005d2bf1d75d8eeacb9a058a9ce45
smartx-web <musabilyamin137@gmail.com>
2026-07-30 16:28:31 +0100

    Create business_repository.go

---
+++ backend/internal/business/repository/business_repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import "github.com/smartx-web/idoma-connect/backend/internal/business/model"
+
+var Businesses = []model.Business{
+	{
+		ID:       1,
+		Name:     "Royal Specialist Hospital",
+		Category: "Hospital",
+		LGA:      "Otukpo",
+		Phone:    "07060784477",
+	},
+}
